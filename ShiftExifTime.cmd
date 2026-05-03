e86557123e0f98ab9bdb2387d9cf3937f373bda0
Roman <rzhestkov@gmail.com>
2026-05-03 22:04:16 +0300

    Изменены комментарии

--- ShiftExifTime.cmd
+++ ShiftExifTime.cmd
@@ -1,9 +1,12 @@
 @echo off
-set "HOURS_OFFSET=2"
-set "MINUTES_OFFSET=0"
-set "SECONDS_OFFSET=0"
+rem Каждый параметр может быть со своим знаком
+set "HOURS_OFFSET=5"
+set "MINUTES_OFFSET=-4"
+set "SECONDS_OFFSET=10"
 
+rem Переход в папку расположения скрипта и запуск
 pushd "%~dp0"
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ShiftExifTime.ps1" -HoursOffset %HOURS_OFFSET% -MinutesOffset %MINUTES_OFFSET% -SecondsOffset %SECONDS_OFFSET% -TargetFolder "%CD%"
-popd
+rem Возврат в текущую папку
+rem popd
 pause
